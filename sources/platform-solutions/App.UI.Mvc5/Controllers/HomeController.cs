c34ff6aba73d06bb7229c2e8dc984b328a0b93c7
akasarto <github@thiagoschneider.com>
2017-11-23 15:53:08 -0200

    View templates

---
+++ sources/platform-solutions/App.UI.Mvc5/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using App.UI.Mvc5.Models;
+using System.Web.Mvc;
+
+namespace App.UI.Mvc5.Controllers
+{
+	[RoutePrefix("")]
+	public class HomeController : __BaseController
+	{
+		[Route(Name = "HomeIndexGet")]
+		public ActionResult Index()
+		{
+			var model = new EmptyViewModel();
+
+			return View(model);
+		}
+	}
+}
--- sources/platform-solutions/App.UI.Mvc5/Models/BaseViewModel.cs
+++ sources/platform-solutions/App.UI.Mvc5/Models/BaseViewModel.cs
@@ -5,16 +5,5 @@ namespace App.UI.Mvc5.Models
 	public abstract class BaseViewModel
 	{
 		public string PageTitle { get; set; } = string.Empty;
-
-		public LayoutOptions LayoutOptions { get; set; }
-
-		public string GetHtmlClasses()
-		{
-			var result = new StringBuilder();
-
-			// Process possible layout options
-
-			return result.ToString();
-		}
 	}
 }
