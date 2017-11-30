8b739cd03565d93ce142dec59d9bf305f22325f2
akasarto <github@thiagoschneider.com>
2017-11-30 01:56:46 -0200

    Scripts review

---
+++ sources/platform-solutions/App.UI.Mvc5/Scripts/_setups/tooltips.setup.js
@@ -0,0 +1,16 @@
+/*
+ * Tooltis
+ * - Bootstrap 4 tooltips
+ */
+
+(function ($, website) {
+
+	'use strict';
+
+	$(function () {
+
+		$('[data-toggle="tooltip"]').tooltip();
+
+	});
+
+}).apply(this, [jQuery, window.website]);
