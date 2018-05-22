60d42dfbc29a8a39ab1d3affa5ef2694a9370a97
weedkiller <weedkiller@users.noreply.github.com>
2018-05-22 01:05:40 -0400

    UsersinRoleVM

---
+++ sources/platform-solutions/App.Core/Entities/UserRoleVieModel.cs
@@ -0,0 +1,9 @@
+//User ROles VM
+public class Users_in_Role_ViewModel  
+    {  
+        public string UserId { get; set; }  
+        public string Username { get; set; }  
+        public string Email { get; set; }  
+        public string Role { get; set; }  
+    }  
+
