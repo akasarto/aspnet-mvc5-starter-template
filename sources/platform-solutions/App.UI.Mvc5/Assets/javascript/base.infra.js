56ebd855b7567ec92ccc6b34a1c76b098f92f610
akasarto <github@thiagoschneider.com>
2018-09-10 01:26:42 -0300

    Managing client libraries with VS LibMan intead of nuget

---
+++ sources/platform-solutions/App.UI.Mvc5/Assets/javascript/base.infra.js
@@ -0,0 +1,9 @@
+
+window.website = {
+
+	events: $({}),
+
+	// Loaded from server (_LayoutBase.cshtml => RenderAdminScriptResourcesTag)
+	resources: window.resources
+
+};
--- sources/platform-solutions/App.UI.Mvc5/Scripts/_base/website.base.js
+++
@@ -1,9 +0,0 @@
-
-window.website = {
-
-	events: $({}),
-
-	// Loaded from server (_layout.cshtml)
-	resources: window.resources
-
-};
