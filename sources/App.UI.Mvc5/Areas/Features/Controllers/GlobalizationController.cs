4a2f9cdae284c755949d13ef84ae16a1df7529af
akasarto <github@thiagoschneider.com>
2018-10-15 00:45:53 -0300

    Features basic info

--- sources/App.UI.Mvc5/Areas/Features/Controllers/GlobalizationController.cs
+++ sources/App.UI.Mvc5/Areas/Features/Controllers/GlobalizationController.cs
@@ -18,25 +18,6 @@ namespace App.UI.Mvc5.Areas.Features.Controllers
 			return View(model);
 		}
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		[Route(Name = "Features_Globalization_Index_Post")]
-		public ActionResult Index(GlobalizationViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				Feedback.AddMessage(FeedbackMessageType.Success, "All fields validated correctly.", "Contratulations!");
-			}
-			else
-			{
-				Feedback.AddMessage(FeedbackMessageType.Error, "Some fields are not valid.", "Oops!");
-			}
-
-			model = BuildGlobalizationViewModel(model);
-
-			return View(model);
-		}
-
 		private GlobalizationViewModel BuildGlobalizationViewModel(GlobalizationViewModel postedModel = null)
 		{
 			var model = new GlobalizationViewModel();
@@ -46,33 +27,6 @@ namespace App.UI.Mvc5.Areas.Features.Controllers
 				// Use a mapper to associate the previously posted information to the new model.
 				model.InjectFrom(postedModel);
 			}
-
-			/*
-			// Combo boxes expect a collection of objects to build the options list.
-			// This could be loaded from a database or any other source. For demonstration purposes we will use the sample enum.
-			var sampleValues = Enum.GetValues(typeof(SamplesEnum)).Cast<SamplesEnum>().Select(r => new
-			{
-				id = r.ToString(),
-				value = r.ToString()
-			}).OrderBy(e => e.id).ToList();
-
-			// This is a list that should be always available when presenting the single select options.
-			model.SampleOptionsSingle = new SelectList(
-				sampleValues,
-				dataValueField: "id", // Specify the fields to be used. Here we will use the ones we created above from the enum.
-				dataTextField: "value",
-				selectedValue: model.SelectedSingle
-			);
-
-			// This is a list that should be always available when presenting the multi select options.
-
-			model.SampleOptionsMulti = new MultiSelectList(
-				sampleValues,
-				dataValueField: "id", // Specify the fields to be used. Here we will use the ones we created above from the enum.
-				dataTextField: "value",
-				selectedValues: model.SelectedMulti
-			);
-			*/
 
 			return model;
 		}
